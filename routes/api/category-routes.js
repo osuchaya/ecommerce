fbca7d345e5474efc53c2dc540921e6981b1d4e5
Suchaya Osatis <suchaya.osatis@gmail.com>
2023-05-01 18:18:14 -0700

    DELETE route for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -46,6 +46,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((deletedCategory) => res.status(200).json(deletedCategory))
+  .catch((err) => res.status(400).json(err));
 });
 
 module.exports = router;
