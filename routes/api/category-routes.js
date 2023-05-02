a896ac53e5ed64f9ef04815d1a055102c37dd89e
Suchaya Osatis <suchaya.osatis@gmail.com>
2023-05-01 18:10:14 -0700

    GET all for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,11 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [Product],
+  })
+    .then((allCategories) => res.json(allCategories))
+    .catch((err) => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
