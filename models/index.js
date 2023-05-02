124345c05e1a3915de1ed3ccd6b55fe429c6a910
Suchaya Osatis <suchaya.osatis@gmail.com>
2023-05-01 18:05:16 -0700

    Product belongsToMany Tags

--- models/index.js
+++ models/index.js
@@ -17,7 +17,10 @@ Category.hasMany(Product, {
 
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
